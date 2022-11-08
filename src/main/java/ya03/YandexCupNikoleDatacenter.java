3d94570a929e20baa640ff8cccade941b15fc3bd
Igor Pets <b_ear@mail.ru>
2022-11-08 22:44:56 +0300

    yandexcupnicolecenter version 2 step 42 break by tl

--- src/main/java/ya03/YandexCupNikoleDatacenter.java
+++ src/main/java/ya03/YandexCupNikoleDatacenter.java
@@ -1,14 +1,17 @@
 package ya03;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
 
 public class YandexCupNikoleDatacenter {
     public static String result = "no_data";
 
-    public static void main(String[] args) {
-        try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+        //try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
             // Число связей между серверами.
             String[] params = reader.readLine().split(" ");
             int datacenter_count = Integer.parseInt(params[0]);
@@ -29,67 +32,69 @@ public class YandexCupNikoleDatacenter {
             result = "";
             // Считываем все события.
             while (events_count-- > 0) {
-                String[] comand_line = reader.readLine().split(" ");
-                String comand = comand_line[0];
-                switch (comand) {
-                    case "DISABLE":
-                        int dis_center_num = Integer.parseInt(comand_line[1]) - 1;
-                        int dis_server_num = Integer.parseInt(comand_line[2]) - 1;
+                String line = reader.readLine();
+                if (line.length() == 6) {
+                    int center_index = -1;
+                    if (line.charAt(5) == 'X') {
+                        // GETMAX
+                        int center_count = -1;
+                        for (int i = 0; i < datacenter_count; i++) {
+                            int temp_live;
+                            if (center_live[i] == -1) {
+                                temp_live = center_wserv_count[i] * center_reset[i];
+                                center_live[i] = temp_live;
+                            } else
+                                temp_live = center_live[i];
+                            if (center_count < temp_live) {
+                                center_index = i;
+                                center_count = temp_live;
+                            }
+                        }
+                    } else {
+                        // GETMIN
+                        int center_count = Integer.MAX_VALUE;
+                        for (int i = 0; i < datacenter_count; i++) {
+                            int temp_live;
+                            if (center_live[i] == -1) {
+                                temp_live = center_wserv_count[i] * center_reset[i];
+                                center_live[i] = temp_live;
+                            } else
+                                temp_live = center_live[i];
+                            if (center_count > temp_live) {
+                                center_index = i;
+                                center_count = temp_live;
+                            }
+                        }
+                    }
+                    result = result + (center_index + 1) + "\n";
+                } else {
+                    if (line.charAt(0) == 'D') {
+                        String[] comand_line = line.substring(8).split(" ");
+                        // DISABLE
+                        int dis_center_num = Integer.parseInt(comand_line[0]) - 1;
+                        int dis_server_num = Integer.parseInt(comand_line[1]) - 1;
                         if (servers[dis_center_num][dis_server_num] == 0) {
                             // Отключаем рабочий сервер.
                             servers[dis_center_num][dis_server_num] = 1;
                             center_wserv_count[dis_center_num]--;
                             center_live[dis_center_num] = -1;
                         }
-                        break;
-                    case "GETMAX":
-                        int center_index_max = -1;
-                        int center_count_max = -1;
-                        for (int i = 0; i < datacenter_count; i++) {
-                            int temp_live;
-                            if (center_live[i] == -1) {
-                                temp_live = center_wserv_count[i] * center_reset[i];
-                                center_live[i] = temp_live;
-                            } else
-                                temp_live = center_live[i];
-                            if (center_count_max < temp_live) {
-                                center_index_max = i;
-                                center_count_max = temp_live;
-                            }
-                        }
-                        result = result + (center_index_max + 1) + "\n";
-                        break;
-                    case "RESET":
-                        int center_reset_num = Integer.parseInt(comand_line[1]) - 1;
+                    } else {
+                        // RESET
+                        int center_reset_num = Integer.parseInt(line.substring(6)) - 1;
                         center_reset[center_reset_num]++;
-                        Arrays.fill(servers[center_reset_num], (byte)0);
+                        Arrays.fill(servers[center_reset_num], (byte) 0);
                         center_wserv_count[center_reset_num] = servers_count;
                         center_live[center_reset_num] = -1;
                         //for (int i = 0; i < servers_count; i++)
                         //    servers[center_reset_num][i] = 0;
-                        break;
-                    case "GETMIN":
-                        int center_index_min = -1;
-                        int center_count_min = Integer.MAX_VALUE;
-                        for (int i = 0; i < datacenter_count; i++) {
-                            int temp_live;
-                            if (center_live[i] == -1) {
-                                temp_live = center_wserv_count[i] * center_reset[i];
-                                center_live[i] = temp_live;
-                            } else
-                                temp_live = center_live[i];
-                            if (center_count_min > temp_live) {
-                                center_index_min = i;
-                                center_count_min = temp_live;
-                            }
-                        }
-                        result = result + (center_index_min + 1) + "\n";
-                        break;
+                    }
                 }
             }
             System.out.println(result);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+            //reader.close();
+       //} catch (Exception e) {
+       //     System.out.println(e.getMessage());
+       // }
     }
 }
--- src/test/java/ya03/YandexCupNikoleDatacenter_test.java
+++ src/test/java/ya03/YandexCupNikoleDatacenter_test.java
@@ -8,25 +8,31 @@ public class YandexCupNikoleDatacenter_test extends TestDriver {
         test.className = "YandexCupNikoleDatacenter";
 
         String[] test_data = {
-                "3 3 12\n" +
+                "3 3 16\n" +
                         "DISABLE 1 2\n" +
                         "DISABLE 2 1\n" +
                         "DISABLE 3 3\n" +
+                        "DISABLE 2 1\n" +
                         "GETMAX\n" +
                         "RESET 1\n" +
                         "RESET 2\n" +
                         "DISABLE 1 2\n" +
                         "DISABLE 1 3\n" +
+                        "DISABLE 1 3\n" +
+                        "DISABLE 1 3\n" +
                         "DISABLE 2 2\n" +
+                        "GETMAX\n" +
                         "GETMAX\n" +
                         "RESET 3\n" +
                         "GETMIN",
-                "2 3 9\n" +
+                "2 3 11\n" +
                         "DISABLE 1 1\n" +
                         "DISABLE 2 2\n" +
+                        "DISABLE 1 1\n" +
                         "RESET 2\n" +
                         "DISABLE 2 1\n" +
                         "DISABLE 2 3\n" +
+                        "RESET 1\n" +
                         "RESET 1\n" +
                         "GETMAX\n" +
                         "DISABLE 2 1\n" +
@@ -35,14 +41,19 @@ public class YandexCupNikoleDatacenter_test extends TestDriver {
         String[] result_data = {
                 "1\n" +
                         "2\n" +
-                        "1",
+                        "2\n" +
+                        "1\n",
                 "1\n" +
-                        "2"
+                        "2\n"
         };
         test.runtest(test_data, result_data, 0);
     }
     protected String work(){
-        YandexCupNikoleDatacenter.main(null);
-        return YandexCupNikoleDatacenter.result;
+        try {
+            YandexCupNikoleDatacenter.main(null);
+            return YandexCupNikoleDatacenter.result;
+        }catch (Exception e){
+            return "";
+        }
     }
 }
